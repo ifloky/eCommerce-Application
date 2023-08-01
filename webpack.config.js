94c455845551e377282f3978bdbab0a23734460c
floky <Petrenko1702@gmail.com>
2023-08-01 23:35:09 +0300

    feat: add any dependences

---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: "./dist",
+    historyApiFallback: true,
+    open: true,
+    compress: true,
+    hot: true,
+    port: 3010,
+  },
+  entry: {
+    main: path.resolve(__dirname, './index.ts')
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.ts']
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: path.resolve(__dirname, 'template.html'),
+      filename: 'index.html',
+      // favicon: './src/assets/fav.ico',
+    }),
+    new CleanWebpackPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  stats: {
+    children: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: "/node_modules/",
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader']
+      },
+      {
+        test: /\.(jpg|png|jpeg|gif)$/,
+        type: "asset/resource",
+      },
+      {
+        test: /\.svg$/,
+        type: "asset/resource",
+        generator: {
+          filename: path.join("icons", "[name].[contenthash][ext]"),
+        },
+      },
+    ],
+  }
+}
