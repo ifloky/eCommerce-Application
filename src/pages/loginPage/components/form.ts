393903506671ab06064d238dccd40116163ec44d
panakir <panakir.webdev@gmail.com>
2023-08-12 01:10:49 +0300

    refactor: refactor file form.ts(Sat, Aug 12, 2023 01:10AM)

--- src/pages/loginPage/components/form.ts
+++ src/pages/loginPage/components/form.ts
@@ -1,65 +1,76 @@
-import { createElement } from "../../../utils/abstract"
+import { createElement } from '../../../utils/abstract';
 
-const createFormElement = (): HTMLFormElement => createElement('form', ['login-page__form', 'form'])
+const createFormElement = (): HTMLFormElement => createElement('form', ['login-page__form', 'form']);
 
-const createWrapper = (): HTMLDivElement => createElement('div', ['form__wrapper'])
+const createWrapper = (): HTMLDivElement => createElement('div', ['form__wrapper']);
 
-const createInputElement = (): HTMLInputElement => createElement('input', ['form__input'])
+const createInputElement = (): HTMLInputElement => createElement('input', ['form__input']);
 
-const createLabelElement = (): HTMLLabelElement => createElement('label', ['form__label'])
+const createLabelElement = (): HTMLLabelElement => createElement('label', ['form__label']);
 
-const createAnchorElement = (): HTMLAnchorElement => createElement('a', ['form__link-button', 'button'])
+const createAnchorElement = (): HTMLAnchorElement => createElement('a', ['form__link-button', 'button']);
 
-const createButtonElement = (): HTMLButtonElement => createElement('button', ['form__button', 'button'])
-
+const createButtonElement = (): HTMLButtonElement => createElement('button', ['form__button', 'button']);
 
 const generateButtonsBlock = (): HTMLDivElement => {
-  const buttonsBlock = createWrapper()
-  buttonsBlock.textContent = 'or'
-  const submitBtn = createButtonElement()
-  submitBtn.textContent = "sign in"
-  const redirectBtn = createAnchorElement()
-  redirectBtn.textContent = 'sign up'
-  buttonsBlock.prepend(submitBtn)
-  buttonsBlock.append(redirectBtn)
-  return buttonsBlock
-}
+  const buttonsBlock = createElement('div', ['form__buttons']);
+  buttonsBlock.textContent = 'or';
+  const submitBtn = createButtonElement();
+  submitBtn.textContent = 'sign in';
+  const redirectBtn = createAnchorElement();
+  redirectBtn.textContent = 'sign up';
+  buttonsBlock.prepend(submitBtn);
+  buttonsBlock.append(redirectBtn);
+  return buttonsBlock;
+};
 
-const generateShowPasswordChecbox = (): HTMLLabelElement => {
-  const label = createLabelElement()
-  const input = createInputElement()
-  input.type = 'checkbox'
-  label.append(input)
-  return label
-}
+const generateShowPasswordChecbox = (): HTMLDivElement => {
+  const wrapper = createWrapper();
+  const label = createLabelElement();
+  label.setAttribute('for', 'toggle');
+  const input = createInputElement();
+  input.setAttribute('id', 'toggle');
+  input.type = 'checkbox';
+  wrapper.append(label, input);
+  wrapper.hidden = true;
+  return wrapper;
+};
 
-const generteEmailField = (): HTMLElement => {
-  const label = createLabelElement()
-  label.textContent = '* Email '
-  const input = createInputElement()
-  input.required = true
-  input.placeholder = 'This is a required field'
-  input.type = 'email'
-  label.append(input)
-  return label
-}
-const genertePasswordField = (): HTMLElement => {
-  const label = createLabelElement()
-  label.innerHTML = '<span>* Password</span> '
-  const input = createInputElement()
-  input.required = true
-  input.type = 'password'
-  input.placeholder = 'This is a required field'
-  label.append(input)
-  return label
-}
+const generteEmailField = (): HTMLDivElement => {
+  const wrapper = createWrapper();
+  const label = createLabelElement();
+  label.setAttribute('for', 'email');
+  label.textContent = '* Email ';
+  const input = createInputElement();
+  input.setAttribute('id', 'email');
+  input.classList.add('input', 'input_email');
+  input.required = true;
+  input.type = 'email';
+  wrapper.append(label, input);
+  return wrapper;
+};
+
+const genertePasswordField = (): HTMLDivElement => {
+  const wrapper = createWrapper();
+  wrapper.classList.add('form__wrapper_password');
+  const label = createLabelElement();
+  label.setAttribute('for', 'password');
+  label.textContent = '* Password';
+  const input = createInputElement();
+  input.classList.add('input', 'input_pass');
+  input.setAttribute('id', 'password');
+  input.required = true;
+  input.type = 'password';
+  const showPassword = generateShowPasswordChecbox();
+  wrapper.append(label, input, showPassword);
+  return wrapper;
+};
 
 export const generateForm = (): HTMLFormElement => {
-  const form = createFormElement()
-  const emailField = generteEmailField()
-  const passwordField = genertePasswordField()
-  const showPassword = generateShowPasswordChecbox()
-  const buttonsBlock = generateButtonsBlock()
-  form.append(emailField, passwordField, showPassword, buttonsBlock)
-  return form
-}
+  const form = createFormElement();
+  const emailField = generteEmailField();
+  const passwordField = genertePasswordField();
+  const buttonsBlock = generateButtonsBlock();
+  form.append(emailField, passwordField, buttonsBlock);
+  return form;
+};
