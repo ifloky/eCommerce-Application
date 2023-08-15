542965cf70c8d7be1b244fc0f0e6358a4c97159a
Viktar <viktarbrutsis@gmail.com>
2023-08-15 16:51:16 +0300

    feat: start receiving data form

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ export async function initializeApp(): Promise<void> {
   try {
     RegistrationPageView()
     showHidePassword()
+    // ReceiveInfoAfterSubmit()
     startRouting()
   } catch (error) {
     // console.error('Error fetching customers:', error);
--- src/pages/registrationPage/RegistrationController.ts
+++ src/pages/registrationPage/RegistrationController.ts
@@ -17,3 +17,52 @@ export function showHidePassword(): void {
 
  }
 
+// interface CustomerRegistrationInfo {
+//   email: string;
+//   password: string;
+//   firstname: string;
+//   lastname: string;
+//   dateOfBirth: string;
+//   street: string;
+//   city: string;
+//   postcode: string;
+//   country: string;
+
+// }
+
+// const registrationInfo: CustomerRegistrationInfo = {
+//   email: '',
+//   password: '',
+//   firstname: '',
+//   lastname: '',
+//   dateOfBirth: '',
+//   street: '',
+//   city: '',
+//   postcode: '',
+//   country: '',
+// }
+
+// export function ReceiveInfoAfterSubmit(): void {
+//   // const submitButton = document.querySelector('.btn-registration') as HTMLButtonElement;
+//   const form = document.querySelector('.registration-form');
+//   const filledForm = document.querySelectorAll('.form-input') as NodeList;
+//   // submitButton.addEventListener('submit', () => {
+//   //   // eslint-disable-next-line no-console
+//   //   console.log(filledForm);
+//   // })
+//   form?.addEventListener('submit', (event: Event) => {
+//      event.preventDefault();
+//      Array.from(filledForm).forEach((elem) => {
+//         registrationInfo.email: Element.value;
+//      });
+//   })
+
+// }
+
+// ReceiveInfoAfterSubmit();
+
+
+
+
+
+
