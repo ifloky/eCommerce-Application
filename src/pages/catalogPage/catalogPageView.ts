6a48bf6f147200fabfdda728ed5fcd2243aa2b62
viktarbrutsis <105799221+viktarbrutsis@users.noreply.github.com>
2023-08-28 16:53:00 +0300

    feat: try to get all id

--- src/pages/catalogPage/catalogPageView.ts
+++ src/pages/catalogPage/catalogPageView.ts
@@ -1,11 +1,43 @@
+/* eslint-disable no-console */
+
 import { fetchBearerToken, DEVELOP_ID, DEVELOP_SECRET } from "../../shared/API";
 
-export function catalogRender(): void {
-   // eslint-disable-next-line no-console
-   fetchBearerToken(DEVELOP_ID, DEVELOP_SECRET);
-   fetch('https://api.us-central1.gcp.commercetools.com/bestshop-rs/products/')
-// eslint-disable-next-line no-console
-.then(response => response.json())
-// eslint-disable-next-line no-console
-.then(json => console.log(json));
+const getAllProductsInfo = async(): Promise<string> => {
+   const response = await fetch('https://api.us-central1.gcp.commercetools.com/bestshop-rs/products?limit=25', {
+      method: 'GET',
+      headers: {
+         'Authorization': `Bearer ${await fetchBearerToken(DEVELOP_ID, DEVELOP_SECRET)}`,
+         'Content-Type': 'application/json',
+      },
+   });
+
+   if (!response.ok) {
+      throw new Error('Failed to get products info');
+   }
+
+    const data = await response.json();
+    return data.results;
 }
+
+export const catalogRender = async(): Promise<void> => {
+   // eslint-disable-next-line no-console
+
+}
+
+// eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
+const allProducts = getAllProductsInfo();
+const allId: string[] = [];
+
+Array(allProducts).forEach(item => {
+   // eslint-disable-next-line no-restricted-syntax
+   for (const key of Object.keys(item)) {
+      if (key === 'id') {
+         allId.push(item[key]);
+      }
+   }
+})
+
+
+// eslint-disable-next-line no-console
+console.log(Array(allId));
+// eslint-disable-next-line no-console
