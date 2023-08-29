b6699700637afa25ff89a6c71ffb497636db1e72
viktarbrutsis <105799221+viktarbrutsis@users.noreply.github.com>
2023-08-29 12:43:22 +0300

    feat: push to switch to Mac

--- src/pages/catalogPage/catalogPageView.ts
+++ src/pages/catalogPage/catalogPageView.ts
@@ -1,10 +1,23 @@
+/* eslint-disable no-console */
 
 import { fetchBearerToken, DEVELOP_ID, DEVELOP_SECRET } from "../../shared/API";
 // import { createElement } from "../../utils/abstract";
+interface ImageDimensions {
+   w: number,
+   h: number,
+}
+interface Image {
+   url: string,
+   dimensions: ImageDimensions,
+}
 
+interface Product {
+   id: string,
+   description: string,
+   images: Image[],
+}
 
-
-const getAllProductsInfo = async(): Promise<string> => {
+const getAllProductsInfo = async(): Promise<Product[]> => {
    const response = await fetch('https://api.us-central1.gcp.commercetools.com/bestshop-rs/products?limit=25', {
       method: 'GET',
       headers: {
@@ -18,6 +31,8 @@ const getAllProductsInfo = async(): Promise<string> => {
    }
 
     const data = await response.json();
+    console.log(data.results[0].masterData.current.masterVariant.images);
+    console.log(Object.keys(data.results[3]));
     return data.results;
 }
 
@@ -35,19 +50,23 @@ export function catalogRender(): HTMLElement {
    <div class="catalog-wrapper-categories-item"><h3 class="catalog-item-title">Other seeds</h3></div>`;
 
    catalogWrapper.append(catalogCategories);
+
    return catalogWrapper;
 }
 // eslint-disable-next-line no-console
 const allProducts = getAllProductsInfo();
+
+console.log(typeof(allProducts));
+
 const allId: string[] = [];
 
 Array(allProducts).forEach(item  => {
    // eslint-disable-next-line no-restricted-syntax
    for (const key of Object.keys(item)) {
-      if (key === 'id') {
-         allId.push(item[key]);
-      }
+       if (key === 'id') {
+         allId.push(item.id);
+       }
    }
 })
 // eslint-disable-next-line no-console
-console.log(Array(allId));
+
