43243f31d0cdbe786a7e7cd8f7a5e902b0248315
Viktar Brutsis <viktarbrutsis@gmail.com>
2023-08-29 09:05:00 +0300

    feat: add catalogPage.scss

--- src/pages/catalogPage/catalogPageView.ts
+++ src/pages/catalogPage/catalogPageView.ts
@@ -1,6 +1,8 @@
-/* eslint-disable no-console */
 
 import { fetchBearerToken, DEVELOP_ID, DEVELOP_SECRET } from "../../shared/API";
+// import { createElement } from "../../utils/abstract";
+
+
 
 const getAllProductsInfo = async(): Promise<string> => {
    const response = await fetch('https://api.us-central1.gcp.commercetools.com/bestshop-rs/products?limit=25', {
@@ -19,16 +21,27 @@ const getAllProductsInfo = async(): Promise<string> => {
     return data.results;
 }
 
-export const catalogRender = async(): Promise<void> => {
-   // eslint-disable-next-line no-console
+export function catalogRender(): HTMLElement {
+   const catalogWrapper: HTMLDivElement = document.createElement('div');
+   const catalogWrapperTitle: HTMLElement = document.createElement('h2');
+   catalogWrapperTitle.classList.add('catalog-wrapper-title');
+   catalogWrapperTitle.innerHTML = `Our seeds catalog`;
+   catalogWrapper.append(catalogWrapperTitle);
+   catalogWrapper.classList.add('catalog-wrapper');
 
+   const catalogCategories: HTMLDivElement = document.createElement('div',);
+   catalogCategories.classList.add('catalog-wrapper-categories');
+   catalogCategories.innerHTML = `<div class="catalog-wrapper-categories-item"><h3 class="catalog-item-title">Tomato, corn, broccoli, coliflower</h3></div>
+   <div class="catalog-wrapper-categories-item"><h3 class="catalog-item-title">Other seeds</h3></div>`;
+
+   catalogWrapper.append(catalogCategories);
+   return catalogWrapper;
 }
-
-// eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
+// eslint-disable-next-line no-console
 const allProducts = getAllProductsInfo();
 const allId: string[] = [];
 
-Array(allProducts).forEach(item => {
+Array(allProducts).forEach(item  => {
    // eslint-disable-next-line no-restricted-syntax
    for (const key of Object.keys(item)) {
       if (key === 'id') {
@@ -36,8 +49,5 @@ Array(allProducts).forEach(item => {
       }
    }
 })
-
-
 // eslint-disable-next-line no-console
 console.log(Array(allId));
-// eslint-disable-next-line no-console
