b440b0d48ed887d623c6ffe8a55025c2a2f8e17a
panakir <panakir.webdev@gmail.com>
2023-08-29 21:37:58 +0300

    fix[getAllTokens]: add token variable (Tue, Aug 29, 2023 21:37)

--- src/pages/loginPage/loginPageModel.ts
+++ src/pages/loginPage/loginPageModel.ts
@@ -10,7 +10,6 @@ export const getAllTokens = async (email: string, password: string): Promise<str
   const queryParams = `?grant_type=password&username=${encodeURIComponent(email)}&password=${encodeURIComponent(password)}`
   const url = `${process.env.BASE_AUTH_URL}/oauth/${process.env.BASE_PROJECT_KEY}/in-store/key=${process.env.BASE_PROJECT_KEY}/customers/token${queryParams}`
   const response = await (await fetch(url, options)).json()
-
   if (response) {
     localStorage.setItem('accessToken', `${response.access_token}`)
     localStorage.setItem('refreshToken', `${response.refresh_token}`)
@@ -19,6 +18,7 @@ export const getAllTokens = async (email: string, password: string): Promise<str
 }
 
 export const checkUser = async (email: string, password: string): Promise<Response> => {
+  const token = localStorage.getItem('accessToken') ? localStorage.getItem('accessToken') : await getAllTokens(email, password)
   const options = {
     method: 'POST',
     body: JSON.stringify({
@@ -26,7 +26,7 @@ export const checkUser = async (email: string, password: string): Promise<Respon
       "password": password
     }),
     headers: {
-      'Authorization': `Bearer ${localStorage.getItem('accessToken')}`,
+      'Authorization': `Bearer ${token}`,
       'Content-Type': 'application/json'
     }
   }
