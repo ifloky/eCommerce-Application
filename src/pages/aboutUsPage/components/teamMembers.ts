d9f356264d48de10ee54cd4a33d9de57d5369f25
viktarbrutsis <105799221+viktarbrutsis@users.noreply.github.com>
2023-09-07 12:54:59 +0300

    feat: change classes name to lowercase and add -primary

--- src/pages/aboutUsPage/components/teamMembers.ts
+++ src/pages/aboutUsPage/components/teamMembers.ts
@@ -6,7 +6,7 @@ export const teamMembers: TeamMember[] = [
     age: 28,
     about: `Master of Science in Engineering, specializing in Metallurgy. Graduated from high school in 2012 and enrolled at GSTU named after P.O. Sukhoy. Completed my master's degree at GSTU named after P.O. Sukhoy in 2019. Worked as a process engineer and a laboratory engineer at JSC "BSW" since 2017. In 2021, I joined LLC "Chislo udachi" as a webmaster.`,
     github: 'https://github.com/ifloky',
-    class: 'Petrenko'
+    class: 'petrenko'
 
   },
   {
@@ -14,13 +14,13 @@ export const teamMembers: TeamMember[] = [
     age: 34,
     about: 'blablabla',
     github: 'https://github.com/panakir',
-    class: 'Kirill'
+    class: 'kirill'
   },
   {
     name: 'Viktar',
     age: 43,
     about: 'Im graduated from Belarus State Economic Unversity in 2005. My specilization is state and municipal management. Since 2006 I\'m working at LLC Tabak-invest as a chief of fashion retail department. I\'m married and have 2 children.',
     github: 'https://github.com/viktarbrutsis',
-    class: 'Brutsis'
+    class: 'brutsis'
   }
 ];
