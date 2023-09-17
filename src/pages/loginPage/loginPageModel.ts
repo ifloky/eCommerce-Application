406428fccd3af0c2785e412fa69cd52cef34a9c1
panakir <panakir.webdev@gmail.com>
2023-09-17 16:43:33 +0300

    refactor: segregate  responseCustomer interface (Sun, Sep 17, 2023 16:43)

--- src/pages/loginPage/loginPageModel.ts
+++ src/pages/loginPage/loginPageModel.ts
@@ -1,36 +1,32 @@
-
-import { postAnonymousFlow, setCookie } from "../../shared/API"
-import { CustomerData } from "../../types/interfaces/customerData"
-
+import { postAnonymousFlow, setCookie } from '../../shared/API';
+import { responseCustomer } from '../../types/interfaces/login';
 
 export const getAllTokens = async (email: string, password: string): Promise<string> => {
-  const credentials = `${process.env.DEVELOP_ID}:${process.env.DEVELOP_SECRET}`
+  const credentials = `${process.env.DEVELOP_ID}:${process.env.DEVELOP_SECRET}`;
   const options = {
     method: 'POST',
     headers: {
-      'Authorization': `Basic ${btoa(credentials)}`,
-      'Content-Type': 'application/json'
-    }
-  }
-  const queryParams = `?grant_type=password&username=${encodeURIComponent(email)}&password=${encodeURIComponent(password)}`
-  const url = `${process.env.BASE_AUTH_URL}/oauth/${process.env.BASE_PROJECT_KEY}/in-store/key=${process.env.BASE_PROJECT_KEY}/customers/token${queryParams}`
-  const response = await (await fetch(url, options)).json()
+      Authorization: `Basic ${btoa(credentials)}`,
+      'Content-Type': 'application/json',
+    },
+  };
+  const queryParams = `?grant_type=password&username=${encodeURIComponent(email)}&password=${encodeURIComponent(
+    password,
+  )}`;
+  const url = `${process.env.BASE_AUTH_URL}/oauth/${process.env.BASE_PROJECT_KEY}/in-store/key=${process.env.BASE_PROJECT_KEY}/customers/token${queryParams}`;
+  const response = await (await fetch(url, options)).json();
   if (response) {
-    setCookie('access_token', response.access_token, 24)
-    setCookie('refresh_token', response.refresh_token, 24 * 30)
+    setCookie('access_token', response.access_token, 48);
+    setCookie('refresh_token', response.refresh_token, 48 * 30);
   }
-  return response.access_token
-}
-
-type responseCustomer = {
-  customer: CustomerData
-}
+  return response.access_token;
+};
 
 export const checkUser = async (email: string, password: string): Promise<responseCustomer> => {
   const body = {
-    "email": email,
-    "password": password
-  }
-  const response: responseCustomer = await postAnonymousFlow('/login', body)
-  return response
-}
+    email: email,
+    password: password,
+  };
+  const response: responseCustomer = await postAnonymousFlow('/login', body);
+  return response;
+};
--- src/widgets/cardProduct/cardProductView.ts
+++ src/widgets/cardProduct/cardProductView.ts
@@ -19,14 +19,19 @@ function cardProductClick(e: Event, cardProductWrapper: HTMLElement, elem: Produ
 export function priceWithDiscount(elem: Product): HTMLElement {
   const cardProductWrapper = createElement('div', ['product-card__wrapper']);
   cardProductWrapper.setAttribute('data-id', elem.id);
-  const price = elem.masterData ? Number(elem.masterData.current.masterVariant.prices[0].value.centAmount) / 100 : Number(elem.masterVariant?.prices[0].value.centAmount) / 100;
-  const discountedPrice = elem.masterData ? Number(elem.masterData.current.masterVariant.prices[0].discounted?.value.centAmount) / 100 : Number(elem.masterVariant?.prices[0].discounted?.value.centAmount) / 100;
-  const image = elem.masterData ? elem.masterData?.current.masterVariant.images[0].url : elem.masterVariant?.images[0].url;
+  const price = elem.masterData
+    ? Number(elem.masterData.current.masterVariant.prices[0].value.centAmount) / 100
+    : Number(elem.masterVariant?.prices[0].value.centAmount) / 100;
+  const discountedPrice = elem.masterData
+    ? Number(elem.masterData.current.masterVariant.prices[0].discounted?.value.centAmount) / 100
+    : Number(elem.masterVariant?.prices[0].discounted?.value.centAmount) / 100;
+  const image = elem.masterData
+    ? elem.masterData?.current.masterVariant.images[0].url
+    : elem.masterVariant?.images[0].url;
   const name = elem.name ? elem.name['en-US'] : elem.masterData?.current.name['en-US'];
   const description = elem.description ? elem.description['en-US'] : elem.masterData?.current.description['en-US'];
   cardProductWrapper?.addEventListener('click', (e): void => cardProductClick(e, cardProductWrapper, elem));
-  cardProductWrapper.innerHTML = `
-  <div class="product-card__image">
+  cardProductWrapper.innerHTML = ` <div class="product-card__image">
     <a href="#">
       <img src="${image}">
       <div class="product-card__shadow"></div>
@@ -42,11 +47,12 @@ export function priceWithDiscount(elem: Product): HTMLElement {
     <h3>"${name}"</h3>
     <p class="product-card__sub-info">"${description}"</p>
     <div class="product-card__price-wrapper">
-      <span class="${discountedPrice ? 'product-card__sale' : 'product-card__price'}">${price ? price + ' $' : ''
-    }</span>
+      <span class="${discountedPrice ? 'product-card__sale' : 'product-card__price'}">${
+        price ? price + ' $' : ''
+      }</span>
       <span class="product-card__price">${discountedPrice ? discountedPrice + ' $' : ''}</span>
     </div>
     <button class="button-light product-card__add-to-cart " id="addToCart">add to cart</button>
-  </div>`
+  </div>`;
   return cardProductWrapper;
 }
