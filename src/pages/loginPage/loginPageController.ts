bf153c3418ef9858b8b54b9c7b3819d22b7cabdd
panakir <panakir.webdev@gmail.com>
2023-09-17 16:56:58 +0300

    fix[validationEmail]: change pattern regExp (Sun, Sep 17, 2023 16:56)

--- src/pages/loginPage/loginPageController.ts
+++ src/pages/loginPage/loginPageController.ts
@@ -1,108 +1,104 @@
-import {
-  redirectToHomePage,
-  redirectToRegisterPage
-} from "../../shared/router"
-import Header from "../../widgets/header/headerView"
-import {
-  checkUser,
-  getAllTokens,
-} from "./loginPageModel"
+import { redirectToHomePage, redirectToRegisterPage } from '../../shared/router';
+import Header from '../../widgets/header/headerView';
+import { checkUser, getAllTokens } from './loginPageModel';
 
 export const isShowed = (event: Event): void => {
-  const { target } = event
-  const passwordField = document.getElementById('password') as HTMLInputElement
+  const { target } = event;
+  const passwordField = document.getElementById('password') as HTMLInputElement;
   if (target instanceof HTMLLabelElement && target.classList.contains('form__label')) {
-    target.classList.toggle('form__label_show-pass')
+    target.classList.toggle('form__label_show-pass');
     if (passwordField.type === 'password') {
-      passwordField.type = 'text'
+      passwordField.type = 'text';
     } else {
-      passwordField.type = 'password'
+      passwordField.type = 'password';
     }
   }
-}
+};
 
 const validationEmail = (target: HTMLInputElement): boolean => {
-  const isValid = false
-  const email = target.value
-  const parent = target.parentElement
-  const emailPattern = /^\S\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})$/
-  const errorText = parent?.querySelector('.form__error-text_email')
+  const isValid = false;
+  const email = target.value;
+  const parent = target.parentElement;
+  const emailPattern = /^\S\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,4})$/;
+  const errorText = parent?.querySelector('.form__error-text_email');
   if (!email.match(emailPattern) && errorText) {
     if (!email.includes('@')) {
-      errorText.textContent = 'Email address must contain an "@" symbol separating local part and domain name.'
+      errorText.textContent = 'Email address must contain an "@" symbol separating local part and domain name.';
     } else {
-      errorText.textContent = 'Email address must be properly formatted (e.g., user@example.com).'
+      errorText.textContent = 'Email address must be properly formatted (e.g., user@example.com).';
     }
-    if (email === '') errorText.textContent = 'Please, enter your email address'
+    if (email === '') errorText.textContent = 'Please, enter your email address';
+  } else if (errorText) {
+    errorText.textContent = '';
   }
-  else if (errorText) { errorText.textContent = '' }
-  if (errorText && errorText.textContent !== '') return isValid
-  return !isValid
-}
+  if (errorText && errorText.textContent !== '') return isValid;
+  return !isValid;
+};
 
 const validationPassword = (target: HTMLInputElement): boolean => {
-  const password = target.value
-  const passwordPattern = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{7,15}$/;
-  const parent = target.parentElement
-  const errorText = parent?.querySelector('.form__error-text_password')
-  const isValid = false
+  const password = target.value;
+  const passwordPattern = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,100}$/;
+  const parent = target.parentElement;
+  const errorText = parent?.querySelector('.form__error-text_password');
+  const isValid = false;
   if (!password.match(passwordPattern) && errorText) {
-    errorText.textContent = 'Password must be at least 8 characters long.Password must contain at least one uppercase letter (A-Z), at least one lowercase letter(a-z), at least one digit(0 - 9), at least one special character (e.g., !@#$%^&*).'
+    errorText.textContent =
+      'Password must be at least 8 characters long.Password must contain at least one uppercase letter (A-Z), at least one lowercase letter(a-z), at least one digit(0 - 9), at least one special character (e.g., !@#$%^&*).';
   } else if (errorText) {
-    errorText.textContent = ''
+    errorText.textContent = '';
   }
-  if (errorText && errorText.textContent !== '') return isValid
-  return !isValid
-}
+  if (errorText && errorText.textContent !== '') return isValid;
+  return !isValid;
+};
 
 export const isValid = (event: Event): void => {
-  const { target } = event
-  let isValidEmail = false
-  let isValidPassword = false
+  const { target } = event;
+  let isValidEmail = false;
+  let isValidPassword = false;
   if (target instanceof HTMLInputElement) {
-    isValidEmail = validationEmail(target)
-    isValidPassword = validationPassword(target)
+    isValidEmail = validationEmail(target);
+    isValidPassword = validationPassword(target);
     if (target.id === 'email') {
-      validationEmail(target)
+      validationEmail(target);
     }
     if (target.id === 'password') {
-      validationPassword(target)
+      validationPassword(target);
     }
   }
-  sessionStorage.setItem('isValid', `${isValidEmail && isValidPassword}`)
-}
+  sessionStorage.setItem('isValid', `${isValidEmail && isValidPassword}`);
+};
 
 export const redirectToRegistrationPage = (event: Event): void => {
-  const { target } = event
+  const { target } = event;
   if (target instanceof HTMLButtonElement && target.type === 'button') {
-    redirectToRegisterPage()
+    redirectToRegisterPage();
   }
-}
+};
 
 export const loginUser = async (event: Event): Promise<void> => {
-  const { target } = event
-  const isValidData = sessionStorage.getItem('isValid')
+  const { target } = event;
+  const isValidData = sessionStorage.getItem('isValid');
   if (isValidData) {
     if (target instanceof HTMLButtonElement && target.type === 'submit') {
-      const emailField = document.getElementById('email')
-      const passwordField = document.getElementById('password')
-      let email = ''
-      let password = ''
+      const emailField = document.getElementById('email');
+      const passwordField = document.getElementById('password');
+      let email = '';
+      let password = '';
       if (emailField instanceof HTMLInputElement && passwordField instanceof HTMLInputElement) {
-        email = emailField.value
-        password = passwordField.value
+        email = emailField.value;
+        password = passwordField.value;
       }
-      event.preventDefault()
-      getAllTokens(email, password)
-      const isCorrectUserData = await (await checkUser(email, password)).customer
+      event.preventDefault();
+      getAllTokens(email, password);
+      const isCorrectUserData = (await checkUser(email, password)).customer;
       if (isCorrectUserData) {
-        const { id } = isCorrectUserData
-        localStorage.setItem('id', id)
-        localStorage.setItem('login', 'true')
+        const { id } = isCorrectUserData;
+        localStorage.setItem('id', id);
+        localStorage.setItem('login', 'true');
         Header.refresh(true);
-        redirectToHomePage()
+        redirectToHomePage();
       }
     }
   }
-  sessionStorage.removeItem('isValid')
-}
+  sessionStorage.removeItem('isValid');
+};
